e04b40f39217f1b06efcfbc2e60d6dfdc2063c8d
hedan <hedan@server.nonobank.com>
2018-03-16 18:14:29 +0800

    接口校验数据对接

--- src/views/testCase/apisInfoCheckComponent.vue
+++ src/views/testCase/apisInfoCheckComponent.vue
@@ -1,94 +1,143 @@
 <template>
-  <el-container style="padding: 40px">
-    <el-row :gutter="20">
-      <el-col :span="9">
-        <!--<p class="vue-transfer-label">接口</p>-->
-        <el-table
-          border
-          :show-header=true
-          highlight-current-row
-          :data="apisInfoCheck">
-          <el-table-column
-            prop="name"
-            label="名称" align="center">
-          </el-table-column>
-          <el-table-column
-            prop="currentBranch"
-            label="当前分支" align="center">
-          </el-table-column>
-          <el-table-column
-            prop="lastBranch"
-            label="最新分支" align="center">
-            <template slot-scope="scope">
-
-              <div v-for="item in scope.row.lastBranch" ><a href="javascript:;" @click="getCheckInfo(item.id,item.branch)">{{item.system}}</a></div>
-
-            </template>
-          </el-table-column>
-        </el-table>
+  <div style="padding:20px;">
+    <el-row style="">
+      <el-col :span="5" style="text-align: center">
+        <div class="grid-content">
+        </div>
       </el-col>
+      <el-col :span="14" style="text-align: center">
+        <div class="grid-content">
+          <!--<p style="text-align: left">接口</p>-->
+          <el-table
+            border
+            :show-header=true
+            highlight-current-row
+            :data="apisInfoCheck">
+            <el-table-column
+              prop="name"
+              label="名称" align="center">
+            </el-table-column>
+            <el-table-column
+              prop="currentBranch"
+              label="当前分支" align="center">
+            </el-table-column>
+            <el-table-column
+              prop="lastBranch"
+              label="最新分支" align="center">
+              <template slot-scope="scope">
 
-      <!--<el-col :span="15">-->
-        <!--<label class="vue-transfer-label">已选接口</label>-->
-        <!--<div style="width:1500px;">-->
-          <!--<el-row :gutter="20">-->
-            <!--<el-col :span="3"><div class="grid-content font-blod"></div></el-col>-->
-            <!--<el-col :span="7"><div class="grid-content font-blod font-center" @click="aa">最新分支</div></el-col>-->
-            <!--<el-col :span="7"><div class="grid-content font-blod font-center">当前分支</div></el-col>-->
-            <!--<el-col :span="7"><div class="grid-content font-blod font-center">对比结果</div></el-col>-->
-          <!--</el-row>-->
-          <!--<el-row :gutter="20">-->
-            <!--<el-col :span="3"><div class="grid-content font-center">请求</div></el-col>-->
-            <!--<el-col :span="7">-->
-              <!--<div class="grid-content">-->
-                <!--<textarea class="json-box"></textarea>-->
-              <!--</div>-->
-            <!--</el-col>-->
-            <!--<el-col :span="7">-->
-              <!--<div class="grid-content">-->
-                <!--<textarea class="json-box"></textarea>-->
-              <!--</div>-->
-            <!--</el-col>-->
-            <!--<el-col :span="7">-->
-              <!--<div class="grid-content">-->
-                <!--<textarea class="json-box"></textarea>-->
-              <!--</div>-->
-            <!--</el-col>-->
-          <!--</el-row>-->
-          <!--<el-row :gutter="20">-->
-            <!--<el-col :span="3"><div class="grid-content font-center">响应</div></el-col>-->
-            <!--<el-col :span="7">-->
-              <!--<div class="grid-content">-->
-                <!--<textarea class="json-box"></textarea>-->
-              <!--</div>-->
-            <!--</el-col>-->
-            <!--<el-col :span="7">-->
-              <!--<div class="grid-content">-->
-                <!--<textarea class="json-box"></textarea>-->
-              <!--</div>-->
-            <!--</el-col>-->
-            <!--<el-col :span="7">-->
-              <!--<div class="grid-content">-->
-                <!--<textarea class="json-box"></textarea>-->
-              <!--</div>-->
-            <!--</el-col>-->
-          <!--</el-row>-->
-        <!--</div>-->
-      <!--</el-col>-->
+                <div v-for="item in scope.row.lastBranch" ><a href="javascript:;" @click="getCheckInfo(scope.row,item.branch)">{{item.branch}}</a></div>
+
+              </template>
+            </el-table-column>
+          </el-table>
+        </div>
+      </el-col>
+      <el-col :span="5" style="text-align: center">
+        <div class="grid-content">
+        </div>
+      </el-col>
     </el-row>
-  </el-container>
+    <el-row>
+      <div id="separate" style="width:100%;height:1;border-bottom:1px solid #ccc;margin:40px 0;"></div>
+      <p style="text-align: center;font-size:16px;font-weight:bolder;padding:10px 0;">已选接口对比信息<p/>
+      <div style="">
+        <el-row :gutter="20">
+          <el-col :span="3"><div class="grid-content font-blod"></div></el-col>
+          <el-col :span="7"><div class="grid-content font-blod font-center">最新分支</div></el-col>
+          <el-col :span="7"><div class="grid-content font-blod font-center">当前分支</div></el-col>
+          <el-col :span="7"><div class="grid-content font-blod font-center">对比结果</div></el-col>
+        </el-row>
+        <el-row :gutter="20">
+          <el-col :span="3"><div class="grid-content font-center">接口请求</div></el-col>
+          <el-col :span="7">
+            <div class="grid-content">
+              <textarea class="json-box">{{jsonData.lastRequestBody}}</textarea>
+            </div>
+          </el-col>
+          <el-col :span="7">
+            <div class="grid-content">
+              <textarea class="json-box">{{jsonData.currentRequestBody}}</textarea>
+            </div>
+          </el-col>
+          <el-col :span="7">
+            <div class="grid-content">
+              <textarea class="json-box">{{jsonData.compareRequest}}</textarea>
+            </div>
+          </el-col>
+        </el-row>
+        <el-row :gutter="20">
+          <el-col :span="3"><div class="grid-content font-center">接口响应</div></el-col>
+          <el-col :span="7">
+            <div class="grid-content">
+              <textarea class="json-box">{{jsonData.lastResponseBody}}</textarea>
+            </div>
+          </el-col>
+          <el-col :span="7">
+            <div class="grid-content">
+              <textarea class="json-box">{{jsonData.currentResponseBody}}</textarea>
+            </div>
+          </el-col>
+          <el-col :span="7">
+            <div class="grid-content">
+              <textarea class="json-box">{{jsonData.compareApiResponse}}</textarea>
+            </div>
+          </el-col>
+        </el-row>
+        <el-row :gutter="20">
+          <el-col :span="3"><div class="grid-content font-center">实际请求</div></el-col>
+          <el-col :span="7">
+            <div class="grid-content">
+              <textarea class="json-box">{{jsonData.requestBody}}</textarea>
+            </div>
+          </el-col>
+          <el-col :span="7">
+            <div class="grid-content">
+
+            </div>
+          </el-col>
+          <el-col :span="7">
+            <div class="grid-content">
+              <textarea class="json-box">{{jsonData.compareRequest}}</textarea>
+            </div>
+          </el-col>
+        </el-row>
+        <el-row :gutter="20">
+          <el-col :span="3"><div class="grid-content font-center">预期结果</div></el-col>
+          <el-col :span="7">
+            <div class="grid-content">
+              <textarea class="json-box">{{jsonData.responseBody}}</textarea>
+            </div>
+          </el-col>
+          <el-col :span="7">
+            <div class="grid-content">
+
+            </div>
+          </el-col>
+          <el-col :span="7">
+            <div class="grid-content">
+              <textarea class="json-box">{{jsonData.compareResponse}}</textarea>
+            </div>
+          </el-col>
+
+        </el-row>
+      </div>
+    </el-row>
+  </div>
 </template>
 
 <script>
+  import ElRow from "element-ui/packages/row/src/row";
 
   export default {
     props: [],
-    components: {},
+    components: {ElRow},
     name: 'apisInfoCheckComponent',
     data(){
-        return {
-          apisInfoCheck:[]
-        }
+      return {
+        apisInfoCheck:[],
+        jsonData:{}
+      }
     },
     created(){
     },
@@ -106,7 +155,7 @@
         // 获取表格内容
         this.$http.get(this.testCaseServer + "testCase/checkCase?testCaseId=" + caseID).then(function (res) {
           if(res.data.code === 10000){
-              console.log(res.data.data)
+            console.log(res.data.data)
             this.apisInfoCheck = res.data.data;
           }
         },function (res) {
@@ -114,56 +163,54 @@
         });
       },
 
-      getCheckInfo(){
+      getCheckInfo(data,branch){
 
-        this.$http.get(this.testCaseServer + "testCaseInterface/checkApi?id="+ 5 +"&apiId="+ 5 + "&lastBranch="+ 8).then(function (res) {
+        this.$http.get(this.testCaseServer + "testCaseInterface/checkApi?id="+ data.id +"&apiId="+ data.apiId + "&lastBranch="+ branch).then(function (res) {
           if(res.data.code === 10000){
             console.log(res.data.data)
+            this.jsonData = res.data.data;
+          }else{
 
           }
         },function (res) {
-
         });
       }
-
-
-
     }
   }
 
 </script>
 
 <style scoped>
-    .el-row {
-        width:100%;
-        margin-bottom: 20px;
-    &:last-child {
-         margin-bottom: 0;
-     }
-    }
-    .grid-content {
-        border-radius: 4px;
-        min-height: 36px;
-        padding-top: 6px;
-    }
-    .font-center{
-        text-align: center;
-    }
-.font-blod{
+  .el-row {
+    /*width:100%;*/
+    margin-bottom: 20px;
+  &:last-child {
+     margin-bottom: 0;
+   }
+  }
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+    padding-top: 6px;
+  }
+  .font-center{
+    text-align: center;
+  }
+  .font-blod{
     font-weight:bolder;
     color:#909399;
-}
-.json-box{
+  }
+  .json-box{
     display: block;
     margin:0;
     width:100%;
-    height:100px;
+    height:200px;
     border:1px solid #ccc;
     border-radius: 4px;
     resize: none;
     outline: none;
-    overflow: scroll;
-}
+    /*overflow: scroll;*/
+  }
   .el-tree {
     margin-right: 2px;
   }

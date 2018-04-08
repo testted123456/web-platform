6352dda213ef0d2ead0ce099f815e0bf95da8dd6
hedan <hedan@server.nonobank.com>
2018-04-08 19:27:21 +0800

    增加 echart

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Login from '@/views/login'
 import Home from './home/homeRouter.js'
 import ApisInfoCheck from '@/views/testCase/apisInfoCheckComponent.vue'
 import AceTest from '@/views/AceTest'
+import Echart from '@/views/echartDemo'
 
 Vue.use(Router)
 
@@ -24,6 +25,11 @@ export default new Router({
       path:'/ace',
       name:'AceTest',
       component:AceTest
+    },
+    {
+      path:'/echart',
+      name:'Echart',
+      component:Echart
     }
   ]
 })
